6ba05c38216b5c3d10a1c4443dc2491e9bb0f6d2
Upcy12 <sanakordukov38@gmail.com>
2024-03-18 19:45:10 +0500

    фвв фсешщт ащк зщоусеы

---
+++ src/routes/project/apps/1/+page.svelte
@@ -0,0 +1,55 @@
+<script>
+    let newItem='';
+    let todoList =[];
+    function add(){
+        if (newItem!==''){
+            todoList=[
+                ...todoList,
+                {
+                    Задача:newItem,
+                    Завершено:false,
+                },
+            ];
+            newItem='';
+        }
+    }
+</script>
+<main>
+    <form on:submit|preventDefault={add}>
+        <input bind:value={newItem} placeholder="Enter to-do"/>
+        <button class="add-todo" on:click={add}><span>+</span></button>
+    </form>
+</main>
+<style>
+main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    min-height: 100%;
+    padding: Svmin;
+    box-sizing: border-box;
+    background: antiquewhite;
+
+}
+
+form {
+    width: 100%;
+    max-width: SO0px;
+    display: flex;
+    align-items: center;
+    margin-bottom: Irem;
+}
+
+input {
+    flex-grow: 1;
+    width: 0;
+    border: none;
+    border-bottom: 1px solid black;
+    background: transparent;
+    box-shadow: none;
+    font-size: 1.2rem;
+    margin: 0;
+    outline: none;
+
+}
+</style>

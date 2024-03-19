e2982aca278a5d7c4198c2f762b9e55f1c26e21d
Upcy12 <sanakordukov38@gmail.com>
2024-03-19 18:28:31 +0500

    fix errros

--- src/components/icon.svelte
+++ src/components/icon.svelte
@@ -9,7 +9,7 @@
     name: 'delete',
     path: '<path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM8 9h8v10H8V9zm7.5-5l-1-1h-51-1 1H5v2h14V4h-3.5z" fill="currentColor"></path>',
     }];
-    let icon = icons.find((item) => item.name === name);
+    let Icon = icons.find((item) => item.name === name);
 
 </script>
 
